d7e4d238062eb321c118509fb38610754c0659d0
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-31 16:45:19 -0400

    added functionality for when the current time is outside of the work day: store list of every line in hosts file, write each line as long as it doesn't contain any of the website URLs, remove everything after pointer to avoid duplication

--- website_blocker.py
+++ website_blocker.py
@@ -11,15 +11,13 @@ website_list = [
     'www.instagram.com', 'instagram.com'
 ]
 
-# infinite loop running every 5 seconds
-# conditional to check if the current time is within the work day (9am-5pm)
+# loop running every 5 sec to check if current time is during the work day (9am-5pm)
 while True:
     if dt(dt.now().year,
           dt.now().month,
           dt.now().day, 9) < dt.now() < dt(dt.now().year,
                                            dt.now().month,
                                            dt.now().day, 17):
-        print('Working hours...go be productive!')
         # open hosts file, append redirect and website URL if not already there
         with open(hosts_path, 'r+') as file:
             content = file.read()
@@ -28,7 +26,18 @@ while True:
                     pass
                 else:
                     file.write(redirect + ' ' + website + '\n')
+        print('Working hours...go be productive!')
     else:
+        # open hosts file, store list of each line, move pointer to beginning
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            # write each line only if it doesn't contain any of the website URLs
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            # remove everything after pointer's location to avoid duplication
+            file.truncate()
         print('Free time...visit any site you would like!')
 
     time.sleep(5)
